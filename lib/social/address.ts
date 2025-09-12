0500eedc763172f3096038cc8d2127224af26b71
Adarsh Dhar <dharadarsh0@gmail.com>
2025-09-12 06:58:45 +0530

    so far

--- lib/social/address.ts
+++
@@ -1,9 +0,0 @@
-// Contract addresses for Social Yield Protocol
-
-export const SOCIAL_YIELD_PROTOCOL_ADDRESS = '0xc3BD0b83a76CA92A343fAD7b9fD6bB37DeC1Da7E' as const
-export const BOOST_NFT_ADDRESS = '0xFA0FcD31714A77fB84B636d68c758dAA51ACA0b2' as const
-export const USDT_TOKEN_ADDRESS = '0x50DBbF87a5aED08BCACa0f9579494A7f74cc3fd2' as const
-
-// Chain configuration
-export const CHAIN_ID = 1001 // Kairos network
-export const RPC_URL = 'https://public-en-kairos.node.kaia.io'
--- lib/social/index.ts
+++
@@ -1,9 +0,0 @@
-// Main exports for Social Yield Protocol
-
-export * from './types'
-export * from './address'
-export * from './abi'
-export * from './service'
-
-// Re-export the main service function for convenience
-export { createSocialYieldProtocolService } from './service'
